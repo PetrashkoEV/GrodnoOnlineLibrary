5fc9b954acc0f57be2fe5afe334a7377262d1da1
PetrashkoEV <petrashkoev@gmail.com>
2014-02-05 21:43:04 +0300

    Carousel home page

--- DigitalResourcesLibrary/DigitalResourcesLibrary/App_Start/BundleConfig.cs
+++ DigitalResourcesLibrary/DigitalResourcesLibrary/App_Start/BundleConfig.cs
@@ -23,21 +23,14 @@ namespace DigitalResourcesLibrary
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/Style.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/Bootstrap/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/Site.css",
+                        "~/Content/themes/bootstrap/bootstrap.css",
+                        "~/Content/Style.css"));
+
         }
     }
 }
