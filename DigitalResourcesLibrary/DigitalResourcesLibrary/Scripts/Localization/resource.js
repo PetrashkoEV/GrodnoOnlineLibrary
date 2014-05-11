53ff0a18152932c77bb13cb9bfc46bac41e424a0
PetrashkoEV <petrashkoev@gmail.com>
2014-05-11 23:46:10 +0300

    fix bug include file localization

---
+++ DigitalResourcesLibrary/DigitalResourcesLibrary/Scripts/Localization/resource.js
@@ -0,0 +1,54 @@
+var messages = { ru: {}, be:{}, en: {} };
+
+messages.ru.resource = {
+    multiselect: {
+        none_selected_tag: "Выберете ключевое слово",
+        n_selected_text_tag: "ключевых слов выбрано",
+
+        none_selected_type_doc: "Выберете тип документа",
+        n_selected_text_type_doc: "типа документов выбрано",
+
+        select_item_category: 'Выберите категорию',
+        
+        select_all: "Выбрать все"
+    },
+    base: {
+        search: 'Поиск'
+    }
+};
+
+messages.be = {};
+messages.be.resource = {
+    multiselect: {
+        none_selected_tag: "Выберете ключевое слово",
+        n_selected_text_tag: "ключевых слов выбрано",
+
+        none_selected_type_doc: "Выберете тип документа",
+        n_selected_text_type_doc: "типа документов выбрано",
+
+        select_item_category: 'Выберите категорию',
+
+        select_all: "Выбрать все"
+    },
+    base: {
+        search: 'Поиск'
+    }
+};
+
+messages.en = {};
+messages.en.resource = {
+    multiselect: {
+        none_selected_tag: 'None selected Tag',
+        n_selected_text_tag: 'Tag selected',
+
+        none_selected_type_doc: 'None selected Type Doc',
+        n_selected_text_type_doc: 'Type Documents selected',
+
+        select_item_category: 'Сhoose a category',
+
+        select_all: 'Select All'
+    },
+    base: {
+        search: 'Search'
+    }
+};
---
+++ DigitalResourcesLibrary/DigitalResourcesLibrary/Scripts/Localization/translate.js
@@ -0,0 +1,30 @@
+var result;
+
+if (typeof settings !== 'object') {
+    var settings = {};
+    result = $.ajax({
+        type: "POST",
+        url: "/Localization/GetCurentLocalization",
+        async: false
+    }).responseText;
+}
+// Default language
+settings.lang = result;
+
+function resx (params, lang) {
+    var lang = lang || settings.lang, translated = '', code;
+    params = params.toLowerCase().split(':');
+    if (messages[lang] !== undefined && params.length) {
+        for (var i = 0, msgcat = messages[lang]; i < params.length; i++) {
+            code = params[i];
+            if (typeof msgcat[code] === 'object') {
+                msgcat = msgcat[code];
+            }
+            if (typeof msgcat[code] === 'string') {
+                translated = msgcat[code];
+                break;
+            }
+        }
+    }
+    return translated;
+};
