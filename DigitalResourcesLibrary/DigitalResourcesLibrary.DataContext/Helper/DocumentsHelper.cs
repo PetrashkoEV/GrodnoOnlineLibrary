1c708c2025657726f335141c0ea0058cebcf73fd
PetrashkoEV <petrashkoev@gmail.com>
2014-05-15 00:12:50 +0300

    Coommit up count news on page

--- DigitalResourcesLibrary/DigitalResourcesLibrary.DataContext/Helper/DocumentsHelper.cs
+++ DigitalResourcesLibrary/DigitalResourcesLibrary.DataContext/Helper/DocumentsHelper.cs
@@ -2,7 +2,7 @@
 {
     public static class DocumentsHelper
     {
-        private static int _count = 1;
+        private static int _count = 10;
 
         public static int CountNewsOnPage
         {
