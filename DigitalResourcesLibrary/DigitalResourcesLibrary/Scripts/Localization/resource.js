32921f757869c982462a520d9ba806664d9dbb9a
PetrashkoEV <petrashkoev@gmail.com>
2014-06-11 21:06:53 +0300

    Commit belarysian localization

--- DigitalResourcesLibrary/DigitalResourcesLibrary/Scripts/Localization/resource.js
+++ DigitalResourcesLibrary/DigitalResourcesLibrary/Scripts/Localization/resource.js
@@ -21,18 +21,18 @@ messages.ru.resource = {
 messages.be = {};
 messages.be.resource = {
     multiselect: {
-        none_selected_tag: "Выберете ключевое слово",
-        n_selected_text_tag: "ключевых слов выбрано",
+        none_selected_tag: "Выберыце ключавое слова",
+        n_selected_text_tag: "ключавых слоў абрана",
 
-        none_selected_type_doc: "Выберете тип документа",
-        n_selected_text_type_doc: "типа документов выбрано",
+        none_selected_type_doc: "Выберыце тып дакумента",
+        n_selected_text_type_doc: "тыпа дакументаў абрана",
 
-        select_item_category: 'Выберите категорию',
+        select_item_category: 'Выберыце катэгорыю',
 
-        select_all: "Выбрать все"
+        select_all: "Выбраць усе"
     },
     base: {
-        search: 'Поиск',
+        search: 'Пошук',
         loading: 'Загрузка ...'
     }
 };
