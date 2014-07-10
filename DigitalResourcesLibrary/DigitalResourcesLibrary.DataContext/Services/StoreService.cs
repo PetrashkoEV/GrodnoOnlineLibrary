61b1d177064f60ecf813af5a4490c0eb8f85a73e
GorBiblGrodno <adm_gor_bibl_grodno@rambler.ru>
2014-07-10 14:38:12 +0300

    Commit test new user

--- DigitalResourcesLibrary/DigitalResourcesLibrary.DataContext/Services/StoreService.cs
+++ DigitalResourcesLibrary/DigitalResourcesLibrary.DataContext/Services/StoreService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DigitalResourcesLibrary.DataContext.Enums;
 using DigitalResourcesLibrary.DataContext.Helper;
 using DigitalResourcesLibrary.DataContext.Interfaces;
